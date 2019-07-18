345a2878fc4ae87fd0b518eb16bee089c81062e6
Nicolas Silveira <46078503+NikoSilveira@users.noreply.github.com>
2019-07-18 18:48:17 -0400

    Added validation to filter commands.

--- Misaki/src/Outfasted/Misaki/Commands.java
+++ Misaki/src/Outfasted/Misaki/Commands.java
@@ -153,32 +153,40 @@ public class Commands extends ListenerAdapter{
 		
 		else if(args[0].equalsIgnoreCase(Main.prefix + "filter")) {		//chat filter
 			
-			if(args[1].equalsIgnoreCase("status")) {		//Check filter status
+			if(args.length == 1) {		//validate input
 				
-				if(filterEnabled == true) {
-					event.getChannel().sendMessage("The chat filter is enabled.").queue();
-				}
-				else if(filterEnabled == false) {
-					event.getChannel().sendMessage("The chat filter is disabled.").queue();
-				}
+				event.getChannel().sendMessage("To use the filter command correctly, type: **--filter status** or **--filter toggle**").queue();
 				
 			}
-			else if(args[1].equalsIgnoreCase("toggle")) {	//Enable/disable filter
+			else {
 				
-				if(filterEnabled == false) {
-					filterEnabled = true;
-					event.getChannel().sendMessage("The filter has been enabled").queue();
+				if(args[1].equalsIgnoreCase("status")) {	//Check filter status
+					
+					if(filterEnabled == true) {
+						event.getChannel().sendMessage("The chat filter is enabled.").queue();
+					}
+					else if(filterEnabled == false) {
+						event.getChannel().sendMessage("The chat filter is disabled.").queue();
+					}
+					
 				}
-				else if(filterEnabled == true) {
-					filterEnabled = false;
-					event.getChannel().sendMessage("The filter has been disabled").queue();
+				else if(args[1].equalsIgnoreCase("toggle")) {	//Enable/disable filter
+					
+					if(filterEnabled == false) {
+						filterEnabled = true;
+						event.getChannel().sendMessage("The filter has been enabled").queue();
+					}
+					else if(filterEnabled == true) {
+						filterEnabled = false;
+						event.getChannel().sendMessage("The filter has been disabled").queue();
+					}
+					
 				}
 				
 			}
 			
+			
 		}
-		
-		
 		
 		
 	}
